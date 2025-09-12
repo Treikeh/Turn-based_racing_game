4321ff6418087049bb227e8d98e5579f82043c16
AndersFaane <anders992009@hotmail.no>
2025-09-12 14:24:21 +0200

    Added Track & tweaked car controller

--- Assets/Scipts/ArcadeCarController.cs
+++ Assets/Scipts/ArcadeCarController.cs
@@ -16,7 +16,7 @@ public class ArcadeCarController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = new Vector3(0, -0.5f, 0); // makes car more stable
+        rb.centerOfMass = new Vector3(0, -0.1f, 0); // makes car more stable
     }
 
     void FixedUpdate()
