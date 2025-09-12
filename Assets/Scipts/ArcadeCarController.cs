017d9f21103eb85f625e3ee9fbf81d0daaa70e3f
CCCCowboy <meggestua@gmail.com>
2025-09-12 15:17:31 +0200

    fixed a bunch of stuff (not)
    title

--- Assets/Scipts/ArcadeCarController.cs
+++ Assets/Scipts/ArcadeCarController.cs
@@ -16,7 +16,7 @@ public class ArcadeCarController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = new Vector3(0, -0.1f, 0); // makes car more stable
+        rb.centerOfMass = new Vector3(0f, -0.1f, 0); // makes car more stable
     }
 
     void FixedUpdate()
